eb5a5af9ae2fc99c6ce47421227d6d343239dfc3
Henrryitam <henrygorgias@gmail.com>
2022-09-10 02:59:00 -0700

    hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
-/*
+/**
  *main- Print_a_string
- *Description: use printf
+ *
  *Return: 0
  */
-
 int main(void)
 {
   printf("%s", "with proper grammer, but the outcome is a piece of art,\n");

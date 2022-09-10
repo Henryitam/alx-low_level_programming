ed08f2bf98454869c44e2ef78a7d8668008d4530
Henrryitam <henrygorgias@gmail.com>
2022-09-10 02:24:39 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - srves as the entry point for the program 
  *
  * Return: By default returns 0
- *\
+ */
  int main(viod)
  {
  	puts("\"Programming is like building a multilingual puzzle");

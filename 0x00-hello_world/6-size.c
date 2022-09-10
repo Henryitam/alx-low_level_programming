6646c8ec98ad5c1341818f0a17247b515c3c6d87
Henrryitam <henrygorgias@gmail.com>
2022-09-10 11:11:27 -0700

    6 size commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints sized of characters
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+  printf("size of char: %zu byte(s)\n", sizeof(char));
+  printf("size of an int: %zu byte(s)\n", sieof(int));
+  printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+  print("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+  printf("size of a float: %zu byte(s)\n", sizeof(floa));
+  return (0);
+}

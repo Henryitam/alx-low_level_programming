ec0ac34df2bd90565bb056701e6720a1ad722c62
Henrryitam <henrygorgias@gmail.com>
2022-09-15 10:26:58 -0700

    5 more no

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ char n, c;
 int i = 0;
 while (i < 10)
 {
-  for (n = 0; n <= 14; n++)
+for (n = 0; n <= 14; n++)
 {
 c = n;
 if (n > 9)
@@ -19,7 +19,7 @@ if (n > 9)
 _putchar('1');
 c = n % 10;
 }
-_putchar('0' + c);	  
+_putchar('0' + c);
 }
 _putchar('\n');
 i++;

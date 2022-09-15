ec2bb317a94ca08f56ab5bcee206139c75db1519
Henrryitam <henrygorgias@gmail.com>
2022-09-15 11:53:29 -0700

    the print diagonal commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,25 +2,25 @@
 
 /**
  * print_diagonal - diagonal lines made of backslashes
- * n: number of \ to be printed
+ * @n: number of \ to be printed
  * Return: void
  */
 
-void print_diagonal(int n)
+void print_diagonal(int n)  
 {
-int i = 0, ii;
-while (i <  & n > 0)
-{
-ii = 0;
-while (ii < i)
-{
-_putchar(' ');
-ii++;
-}
-_putchar('\\');
-_putchar('\n');
-i++;
-} 
-if (i == 0)
-_putchar('\n');
+  int i = 0, ii;
+  while (i < n && n > 0)
+    {
+      ii = 0;
+      while (ii < i)
+	{
+	  _putchar(' ');
+	  ii++;
+	}
+      _putchar('\\');
+      _putchar('\n');
+      i++;
+    }
+  if (i == 0)
+    _putchar('\n');
 }

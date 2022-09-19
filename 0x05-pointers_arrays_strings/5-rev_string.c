723b07315bccbd96b3d54266c56f134f97137512
Henrryitam <henrygorgias@gmail.com>
2022-09-19 16:11:40 -0700

    the s rev commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,14 @@ void rev_string(char *s)
 int len, i, half;
 char temp;
 for (len = 0; s[len] != '\0'; len++)
-;  
+;
 i = 0;
 half = len / 2;
-while (half--)    
-{      
+while (half--)
+
 temp = s[len - i - 1];
 s[len - i - 1] = s[i];
 s[i] = temp;
 i++;
-}
-  
+}  
 }

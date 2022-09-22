7a1368a0027d540de7148a434c930b47e4d3945c
Henrryitam <henrygorgias@gmail.com>
2022-09-22 05:00:57 -0700

    the strcat commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
 
 /**
-
  * _strcat - function to concertenate strings
-
  * @:dest:destination string recieving the strings
-
  * @:souce string
-
  * Return:dest
-
  */
 
-
-
 char *_strcat(char *dest, char *src)
-  
 {
-  
-  int k, p;
-  p = 0;
-  while (dest[p] != '\0')  
-    {
-      p++;
-    }
-  k = 0;
-  while (src[k] != '\0')
-    {
-      dest[p] = src[k];
-      
-      p++;
-      k++;
-    }
-  dest[p] = '\0'; 
-  return (dest);
+int k, p;
+p = 0;
+while (dest[p] != '\0')
+{
+p++;
+}
+k = 0;
+while (src[k] != '\0')
+{
+dest[p] = src[k];
+p++;
+k++;
+}
+dest[p] = '\0';
+return (dest);
 }

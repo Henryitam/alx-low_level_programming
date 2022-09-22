e379d0989c924cd4398bdb1b68c3f563bb09d40d
Henrryitam <henrygorgias@gmail.com>
2022-09-22 05:17:25 -0700

    the strncat1 commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * *_strncat - function to concertena a number of strings
  * to a destination string
@@ -12,20 +11,21 @@
  */
 
 char *_strncat(char *dest, char *src, int n)
-{
-int p, k;
-p = 0;
-while (dest[p] != '\0')
-{
-p++;
-}
-k = 0;
-while (src[k] != '\0' && k < n)
-{
-dest[p] = src[k];
-k++;
-p++;
-}
-dest[p] = '\0';
-return (dest);
+  
+{  
+  int p, k; 
+  p = 0;
+  while (dest[p] != '\0')
+    {
+      p++; 
+    }
+  k = 0;
+  while (src[k] != '\0' && k < n)  
+    {
+      dest[p] = src[k]; 
+      k++;
+      p++;
+    }
+  dest[p] = '\0';
+  return (dest);
 }
